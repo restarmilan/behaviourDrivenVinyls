1191154eefe409ff448a2f02fa0f4b21bb9b153d
restarmilan <restarmilan@gmail.com>
2020-02-11 22:42:09 +0100

    initialize WebDriver, WebdriverWait, PageFactory in constructor

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,6 +1,21 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
 
+    final int TIMEOUT = 10;
+    WebDriver driver;
+    WebDriverWait wait;
+    String baseUrl = "https://www.discogs.com/";
 
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, TIMEOUT);
+        AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(this.driver, TIMEOUT);
+        PageFactory.initElements(factory, this);
+    }
 }
