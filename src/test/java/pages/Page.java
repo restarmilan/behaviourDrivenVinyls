5be08c5e27584a6e798255124b671e3c6dc9ba51
restarmilan <restarmilan@gmail.com>
2020-02-12 14:57:20 +0100

    create common method for interacting with a webelement

---
+++ src/test/java/pages/Page.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class Page {
+
+    final int TIMEOUT = 10;
+    WebDriver driver;
+    WebDriverWait wait;
+    String baseUrl = "https://www.discogs.com/";
+
+    public Page(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, TIMEOUT);
+        AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(this.driver, TIMEOUT);
+        PageFactory.initElements(factory, this);
+    }
+
+    public void navigateTo(String urlExtension){
+        driver.get(baseUrl+urlExtension);
+    }
+
+    public void clickOnWebElement(WebElement element) {
+          element.click();
+        }
+}
