18f273e80c0ecc73e854edf97bdac7d5b82339ac
restarmilan <restarmilan@gmail.com>
2020-02-22 16:41:31 +0100

    change generated snippets to camelcase

--- src/test/java/behaviourDrivenVinyls/UserAccessStepDefinitions.java
+++ src/test/java/behaviourDrivenVinyls/UserAccessStepDefinitions.java
@@ -13,24 +13,24 @@ public class UserAccessStepDefinitions extends BaseStepDefs {
     private LoginPage loginPage = new LoginPage(driver);
 
     @Given("I'm on the Discogs.com page as a registered user")
-    public void i_m_on_the_Discogs_com_page_as_a_registered_user() {
+    public void iAmOnTheDiscogsComPageAsARegisteredUser() {
         basePage.navigateTo("");
     }
 
-    @Given("I click on the Log In button")
-    public void i_click_on_the_Log_In_button() {
+    @When("I click on the Log In button")
+    public void iClickOnTheLogInButton() {
         basePage.clickOnLoginButton();
     }
 
     @When("I set the correct {string} and {string}")
-    public void i_set_the_correct_credentials(String username, String password) {
+    public void iSetTheCorrectCredentials(String username, String password) {
         username = System.getenv("DISCOGS_USERNAME");
         password = System.getenv("DISCOGS_PASSWORD");
         loginPage.setLoginCredentials(username, password);
     }
 
     @Then("I should see my profile after successful login")
-    public void i_should_see_my_profile_after_successful_login() {
+    public void iShouldSeeMyProfileAfterSuccessfulLogin() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
