79a2750629e9772e201c731dc9ed8760e8a0409c
restarmilan <restarmilan@gmail.com>
2020-02-23 15:41:34 +0100

    update ChromeDriver version

--- src/test/java/behaviourDrivenVinyls/BaseStepDefs.java
+++ src/test/java/behaviourDrivenVinyls/BaseStepDefs.java
@@ -8,13 +8,11 @@ public class BaseStepDefs {
     WebDriver driver;
     String chromeDriverPath;
 
-    public BaseStepDefs(){
-        this.chromeDriverPath = "/home/rmilan/codecool/test_automation/4si/jiracucumber/src/test/resources/chromedriver.exe";
+    public BaseStepDefs() {
+        this.chromeDriverPath = "/home/rmilan/codecool/test_automation/chromedriver";
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
         this.driver = new ChromeDriver();
     }
-
-
 
 
 }
