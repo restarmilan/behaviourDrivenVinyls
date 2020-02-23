3aa3874c1f5d82e1db83693ea8b3037cea4e4c0c
restarmilan <restarmilan@gmail.com>
2020-02-24 00:28:07 +0100

    add cucumber.xml, spring Config class

---
+++ src/test/java/com/rmilan/behaviourDrivenVinyls/config/Config.java
@@ -0,0 +1,11 @@
+package com.rmilan.behaviourDrivenVinyls.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("application.properties")
+@ComponentScan("com.rmilan.behaviourDrivenVinyls")
+public class Config {
+}
+
+
