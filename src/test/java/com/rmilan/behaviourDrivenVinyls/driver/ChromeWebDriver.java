8ebf6a749036ef78acdfef8e2206c6e185c9e666
restarmilan <restarmilan@gmail.com>
2020-03-06 21:52:17 +0100

    run scenarios with headless chromedriver

--- src/test/java/com/rmilan/behaviourDrivenVinyls/driver/ChromeWebDriver.java
+++ src/test/java/com/rmilan/behaviourDrivenVinyls/driver/ChromeWebDriver.java
@@ -2,6 +2,7 @@ package com.rmilan.behaviourDrivenVinyls.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -14,7 +15,12 @@ public class ChromeWebDriver {
     public WebDriver getDriver() {
         String chromeDriverPath = "/home/rmilan/codecool/test_automation/chromedriver";
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("disable-gpu");
+        chromeOptions.addArguments("window-size=1400,2100");
+        WebDriver driver = new ChromeDriver(chromeOptions);
         return driver;
     }
 }
