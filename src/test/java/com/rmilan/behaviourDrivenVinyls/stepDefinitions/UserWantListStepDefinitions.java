6b8f13dafea2d560a6dc99a1b2123b1d6cdd52a7
restarmilan <restarmilan@gmail.com>
2020-03-17 13:44:26 +0100

    add pending step definitions for 'user want list' scenarios

---
+++ src/test/java/com/rmilan/behaviourDrivenVinyls/stepDefinitions/UserWantListStepDefinitions.java
@@ -0,0 +1,141 @@
+package com.rmilan.behaviourDrivenVinyls.stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class UserWantListStepDefinitions {
+
+    @Given("I select a specific record by url")
+    public void iSelectASpecificRecordByUrl() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I add this record to my want list")
+    public void iAddThisRecordToMyWantList() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should see a confirmation message about addition")
+    public void iShouldSeeAConfirmationMessageAboutAddition() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should see the record on my want list")
+    public void iShouldSeeTheRecordOnMyWantList() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I have a record in my want list")
+    public void iHaveARecordInMyWantList() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I am on the record's page")
+    public void iAmOnTheRecordSPage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should see that this record is in my want list")
+    public void iShouldSeeThatThisRecordIsInMyWantList() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I add note to this record listed in my want list")
+    public void iAddNoteToThisRecordListedInMyWantList() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should see this note on the given want list item")
+    public void iShouldSeeThisNoteOnTheGivenWantListItem() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I set a note for this record")
+    public void iSetANoteForThisRecord() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I click on the artist's name at want list listing")
+    public void iClickOnTheArtistSNameAtWantListListing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should be redirected to the artist's page")
+    public void iShouldBeRedirectedToTheArtistSPage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I click on the records's title at want list listing")
+    public void iClickOnTheRecordsSTitleAtWantListListing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should be redirected to the given record's page")
+    public void iShouldBeRedirectedToTheGivenRecordSPage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I click on the ForSale field at want list listing")
+    public void iClickOnTheForSaleFieldAtWantListListing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should be redirected to the available copies' page")
+    public void iShouldBeRedirectedToTheAvailableCopiesPage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("The available copies' number equals the amount of want list listing")
+    public void theAvailableCopiesNumberEqualsTheAmountOfWantListListing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I remove this item")
+    public void iRemoveThisItem() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("The record should disappear from my want list")
+    public void theRecordShouldDisappearFromMyWantList() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I click on the remove button")
+    public void iClickOnTheRemoveButton() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select Large Covers displaying option")
+    public void iSelectLargeCoversDisplayingOption() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("The added notes to want list items aren't displaying")
+    public void theAddedNotesToWantListItemsArenTDisplaying() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
